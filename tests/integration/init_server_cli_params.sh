ff490a280de7a19a28d8b956a15cd29eb7ae2194
Kim Ngo <kngo@nvidia.com>
2022-06-02 11:21:22 -0700

    Change ssl flags to have consistent dilimeter

--- tests/integration/init_server_cli_params.sh
+++ tests/integration/init_server_cli_params.sh
@@ -7,8 +7,8 @@ else
   server_args="--server ${SERVER}"
 fi
 if [ ! -z "${USE_SSL}" ] && [ "${USE_SSL}" != 0 ]; then
-  server_args="${server_args} --use_ssl"
+  server_args="${server_args} --use-ssl"
 fi
 if [ ! -z "${SSL_CERT}" ]; then
-  server_args="${server_args} --ssl_cert ${SSL_CERT}"
+  server_args="${server_args} --ssl-cert ${SSL_CERT}"
 fi
