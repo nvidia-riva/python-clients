81a6ae922889772a05e897291927476cfc9a6dc9
PeganovAnton <peganoff2@mail.ru>
2022-07-11 18:46:41 +0300

    Redirect to nvidia-riva-client
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ riva_api/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright (c) 2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+raise ValueError(
+    "riva_api is deprecated. If you see this message, then you probably installed the package with command "
+    "`pip install riva-api`. This command includes installation of latest version of `nvidia-riva-client` package."
+    "For info about `nvidia-riva-client` package refer to https://github.com/nvidia-riva/python-clients."
+)
--- tests/integration/prepare_test_output_dir.sh
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: MIT
-
-script_name_="$2"
-test_output_dir="$1/outputs/test_${script_name_%.*}"
-mkdir -p "${test_output_dir}"
-rm -rf "${test_output_dir}"/*
