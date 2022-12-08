4fa75c5c1a0b0c3dcd9ac18d7524652f75ea7d4b
Rahul Mittal <rmittal@nvidia.com>
2022-12-08 17:59:49 +0530

    Update version to 2.9.0

--- riva/client/package_info.py
+++ riva/client/package_info.py
@@ -1,9 +1,9 @@
 # SPDX-FileCopyrightText: Copyright (c) 2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: MIT
 
-MAJOR = 0
-MINOR = 0
-PATCH = 6
+MAJOR = 2
+MINOR = 9
+PATCH = 0
 PRE_RELEASE = 'rc0'
 
 # Use the following formatting: (major, minor, patch, pre-release)
@@ -21,6 +21,6 @@ __download_url__ = 'hhttps://github.com/nvidia-riva/python-clients/releases'
 __description__ = "Python implementation of the Riva Client API"
 __license__ = 'MIT'
 __keywords__ = 'deep learning, machine learning, gpu, NLP, ASR, TTS, NMT, nvidia, speech, language, Riva, client'
-__riva_version__ = "2.7.0"
-__riva_release__ = "22.10"
-__riva_models_version__ = "2.7.0"
+__riva_version__ = "2.9.0"
+__riva_release__ = "23.01"
+__riva_models_version__ = "2.9.0"
