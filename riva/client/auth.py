59cb714a1df322feea3b3a54fc5d8c06b6368261
apeganov <apeganov@nvidia.com>
2022-12-14 19:57:43 +0400

    Add channel options parameter to Auth constructor
    Signed-off-by: apeganov <apeganov@nvidia.com>

--- riva/client/auth.py
+++ riva/client/auth.py
@@ -9,7 +9,10 @@ import grpc
 
 
 def create_channel(
-    ssl_cert: Optional[Union[str, os.PathLike]] = None, use_ssl: bool = False, uri: str = "localhost:50051",
+    ssl_cert: Optional[Union[str, os.PathLike]] = None,
+    use_ssl: bool = False,
+    uri: str = "localhost:50051",
+    options: Optional[List[Tuple[str, Any]]] = None
 ) -> grpc.Channel:
     if ssl_cert is not None or use_ssl:
         root_certificates = None
@@ -18,9 +21,9 @@ def create_channel(
             with open(ssl_cert, 'rb') as f:
                 root_certificates = f.read()
         creds = grpc.ssl_channel_credentials(root_certificates)
-        channel = grpc.secure_channel(uri, creds)
+        channel = grpc.secure_channel(uri, creds, options=options)
     else:
-        channel = grpc.insecure_channel(uri)
+        channel = grpc.insecure_channel(uri, options=options)
     return channel
 
 
@@ -30,6 +33,7 @@ class Auth:
         ssl_cert: Optional[Union[str, os.PathLike]] = None,
         use_ssl: bool = False,
         uri: str = "localhost:50051",
+        channel_options: Optional[List[Tuple[str, Any]]] = None,
     ) -> None:
         """
         A class responsible for establishing connection with a server and providing security metadata.
@@ -40,11 +44,16 @@ class Auth:
             use_ssl (:obj:`bool`, defaults to :obj:`False`): whether to use SSL. If :param:`ssl_cert` is :obj:`None`,
                 then SSL is still used but with default credentials.
             uri (:obj:`str`, defaults to :obj:`"localhost:50051"`): a Riva URI.
+            channel_options (:obj:`Optional[List[Tuple[str, Any]]]`, defaults to :obj:`None`): a list of tuples with
+                key-value pairs (`channel_arguments
+                <https://grpc.github.io/grpc/python/glossary.html#term-channel_arguments>`_ in gRPC Core runtime) to
+                configure the channel.
         """
         self.ssl_cert: Optional[Path] = None if ssl_cert is None else Path(ssl_cert).expanduser()
         self.uri: str = uri
         self.use_ssl: bool = use_ssl
-        self.channel: grpc.Channel = create_channel(self.ssl_cert, self.use_ssl, self.uri)
+        self.options: Optional[List[Tuple[str, Any]]] = channel_options
+        self.channel: grpc.Channel = create_channel(self.ssl_cert, self.use_ssl, self.uri, channel_options)
 
     def get_auth_metadata(self) -> List[Tuple[str, str]]:
         """
