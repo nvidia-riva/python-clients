937360d40495f87a67f3e352a4d3d94002f1934d
Viraj Karandikar <16838694+virajkarandikar@users.noreply.github.com>
2023-01-31 15:45:24 +0530

    Update version (#35)

--- riva/client/package_info.py
+++ riva/client/package_info.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MIT
 
 MAJOR = 2
-MINOR = 9
+MINOR = 10
 PATCH = 0
 PRE_RELEASE = 'rc0'
 
@@ -21,6 +21,6 @@ __download_url__ = 'hhttps://github.com/nvidia-riva/python-clients/releases'
 __description__ = "Python implementation of the Riva Client API"
 __license__ = 'MIT'
 __keywords__ = 'deep learning, machine learning, gpu, NLP, ASR, TTS, NMT, nvidia, speech, language, Riva, client'
-__riva_version__ = "2.9.0"
-__riva_release__ = "23.01"
-__riva_models_version__ = "2.9.0"
+__riva_version__ = "2.10.0"
+__riva_release__ = "23.02"
+__riva_models_version__ = "2.10.0"
