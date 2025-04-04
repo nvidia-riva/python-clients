db9d6c33820230f13afe9f6c11dee8eff48b4e6f
ydharavath <ydharavath@nvidia.com>
2025-04-04 21:15:55 +0530

    Add support to set max length variation for NMT client (#124)
    * Max length variation added to client
    
    * gitmodules and description changed.

--- scripts/nmt/nmt.py
+++ scripts/nmt/nmt.py
@@ -78,6 +78,7 @@ def parse_args() -> argparse.Namespace:
     )
     inputs.add_argument("--text-file", type=str, help="Path to file for translation")
     parser.add_argument("--dnt-phrases-file", type=str, help="Path to file which contains dnt phrases and custom translations")
+    parser.add_argument("--max-len-variation", type=str, help="Parameter to control the maximum variation between the length of source and translated text in terms of tokens")
     parser.add_argument("--model-name", default="", type=str, help="model to use to translate")
     parser.add_argument(
         "--source-language-code", type=str, default="en-US", help="Source language code (according to BCP-47 standard)"
@@ -105,6 +106,7 @@ def main() -> None:
                 target_language=args.target_language_code,
                 future=False,
                 dnt_phrases_dict=dnt_phrases_input,
+                max_len_variation=args.max_len_variation,
             )
             for translation in response.translations:
                 print(translation.text)
