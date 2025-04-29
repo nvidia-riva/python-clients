62b7dff5767f855100ebae8ece1e8864b0a9b9b8
Viraj Karandikar <vkarandikar@nvidia.com>
2025-04-29 11:57:47 +0530

    update version to 2.19.1

--- riva/client/package_info.py
+++ riva/client/package_info.py
@@ -3,7 +3,7 @@
 
 MAJOR = 2
 MINOR = 19
-PATCH = 0
+PATCH = 1
 PRE_RELEASE = 'rc0'
 
 # Use the following formatting: (major, minor, patch, pre-release)
@@ -21,6 +21,6 @@ __download_url__ = 'hhttps://github.com/nvidia-riva/python-clients/releases'
 __description__ = "Python implementation of the Riva Client API"
 __license__ = 'MIT'
 __keywords__ = 'deep learning, machine learning, gpu, NLP, ASR, TTS, NMT, nvidia, speech, language, Riva, client'
-__riva_version__ = "2.19.0"
-__riva_release__ = "25.02"
+__riva_version__ = "2.19.1"
+__riva_release__ = "25.04"
 __riva_models_version__ = "2.19.0"
