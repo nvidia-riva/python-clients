3d084fe08cb9ddbd409a6845fcdd5e189ea7c31a
rmittal-github <61574997+rmittal-github@users.noreply.github.com>
2025-09-24 16:07:13 +0530

    Update package_info.py (#160)

--- riva/client/package_info.py
+++ riva/client/package_info.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MIT
 
 MAJOR = 2
-MINOR = 22
+MINOR = 23
 PATCH = 0
 PRE_RELEASE = 'rc0'
 
@@ -21,6 +21,6 @@ __download_url__ = 'hhttps://github.com/nvidia-riva/python-clients/releases'
 __description__ = "Python implementation of the Riva Client API"
 __license__ = 'MIT'
 __keywords__ = 'deep learning, machine learning, gpu, NLP, ASR, TTS, NMT, nvidia, speech, language, Riva, client'
-__riva_version__ = "2.22.0"
-__riva_release__ = "25.08"
-__riva_models_version__ = "2.22.0"
+__riva_version__ = "2.23.0"
+__riva_release__ = "25.10"
+__riva_models_version__ = "2.23.0"
