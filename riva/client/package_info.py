b099a3617a34e0fe1bf19282f566f8c131aeaf0d
rmittal-github <61574997+rmittal-github@users.noreply.github.com>
2026-03-02 16:20:35 +0530

    Update TTS client to support streaming input (#169)
    * Update TTS client to support streaming input (#164)
    
    * Update version (#167)
    
    * Updating version
    
    * fix release version
    
    ---------
    
    Co-authored-by: rmittal-github <61574997+rmittal-github@users.noreply.github.com>
    
    * Fix stream handling in offline case (#168)
    
    * Updating version
    
    * fix release version
    
    * Handling text in offline case
    
    * Handling text in offline case
    
    * Handling text in offline case
    
    ---------
    
    Co-authored-by: rmittal-github <61574997+rmittal-github@users.noreply.github.com>
    
    * Update SHA of common repo
    
    ---------
    
    Co-authored-by: mohnishparmar <109233781+mohnishparmar@users.noreply.github.com>
    Co-authored-by: Anurag <158568080+atomer-nvidia@users.noreply.github.com>

--- riva/client/package_info.py
+++ riva/client/package_info.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MIT
 
 MAJOR = 2
-MINOR = 24
+MINOR = 25
 PATCH = 0
 PRE_RELEASE = 'rc0'
 
@@ -21,6 +21,6 @@ __download_url__ = 'hhttps://github.com/nvidia-riva/python-clients/releases'
 __description__ = "Python implementation of the Riva Client API"
 __license__ = 'MIT'
 __keywords__ = 'deep learning, machine learning, gpu, NLP, ASR, TTS, NMT, nvidia, speech, language, Riva, client'
-__riva_version__ = "2.24.0"
-__riva_release__ = "25.12"
-__riva_models_version__ = "2.24.0"
+__riva_version__ = "2.25.0"
+__riva_release__ = "26.02"
+__riva_models_version__ = "2.25.0"
